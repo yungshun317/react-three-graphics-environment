558a7f8bdf7222e9bafb98e26a182f4e8d19ec42
yungshun317 <seraph317@gmail.com>
2023-12-25 06:17:56 +0800

    [FEAT] Create `index.html` and `styles.css`

---
+++ src/Scene.js
@@ -0,0 +1,5 @@
+const Scene = () => {
+
+}
+
+export default Scene;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import { Canvas } from "@react-three/fiber";
+import Scene from "./Scene";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <>
+        <Canvas
+            shadows
+            camera={{
+                fov: 75,
+                near: 0.1,
+                far: 100,
+                position: [1, 3, 6],
+            }}
+        >
+            <Scene />
+        </Canvas>
+    </>
+);
