fa92a804c5091f57e9502b1f4b1123dcb1adfc03
yungshun317 <seraph317@gmail.com>
2023-12-25 06:46:00 +0800

    [FEAT] Set `castShadow` and `receiveShadow` props to objects

--- src/Scene.js
+++ src/Scene.js
@@ -1,5 +1,45 @@
-const Scene = () => {
+import {Lightformer, OrbitControls, useHelper} from "@react-three/drei";
+import {useControls} from "leva";
+import {useRef} from "react";
+import * as THREE from "three";
 
+const Scene = () => {
+    const directionaLight = useRef();
+    useHelper(directionaLight, THREE.DirectionalLightHelper, 1);
+    console.log(directionaLight);
+
+    const { meshIntensity } = useControls("meshIntensity", {
+        meshIntensity: { value: 1, min: 0, max: 5 },
+    });
+
+    return (
+        <>
+            <ambientLight />
+            <directionalLight
+                ref={directionaLight} shadow-camera-left={-20}
+                castShadow
+            />
+
+            <OrbitControls/>
+
+            <mesh position-z={-1} scale={5}>
+                <planeGeometry/>
+                <meshBasicMaterial color="orange" />
+            </mesh>
+
+            <Lightformer position-z={-1} scale={5} color="orange" intensity={5} />
+
+            <mesh castShadow position-y={1}>
+                <boxGeometry/>
+                <meshStandardMaterial color="#C7CAC7" envMapIntensity={meshIntensity} />
+            </mesh>
+
+            <mesh receiveShadow position-y={0} rotation-x={-Math.PI * 0.5}>
+                <planeGeometry args={[8, 8]} />
+                <meshStandardMaterial color="#CC3941" />
+            </mesh>
+        </>
+    );
 }
 
 export default Scene;
